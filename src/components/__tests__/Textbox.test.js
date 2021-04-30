0dae8bd8ce8496fbc21bdbc16ff02bbc6309b04c
Jasche7 <jasonc1172@gmail.com>
2021-04-29 22:51:32 -0700

    Added basic UI test for Textbox

--- src/components/__tests__/Textbox.test.js
+++ src/components/__tests__/Textbox.test.js
@@ -3,6 +3,28 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe(`The Textbox component`, () => {
+  it(`includes all of its form inputs`, () => {
+    const { queryByLabelText } = render(<Textbox />);
+
+    const text = queryByLabelText(/Text/);
+    expect(text).toBeTruthy();
+    expect(text.textContent).toBe("");
+
+    const min = queryByLabelText(/Min Occurrences:/);
+    expect(min).toBeTruthy();
+
+    const max = queryByLabelText(/Max Phrase Length:/);
+    expect(max).toBeTruthy();
+
+    const caseSensitive = queryByLabelText(/Case Sensitive/);
+    expect(caseSensitive).toBeTruthy();
+    expect(caseSensitive.checked).toEqual(false);
+
+    const ignoresPunctuation = queryByLabelText(/Ignore Punctuation/);
+    expect(ignoresPunctuation).toBeTruthy();
+    expect(ignoresPunctuation.checked).toEqual(false);
+  });
+
   it(`can submit by clicking the Submit button`, async () => {
     const handleClick = jest.fn();
     const setRequestBody = jest.fn();
@@ -13,11 +35,5 @@ describe(`The Textbox component`, () => {
     userEvent.click(screen.getByText(/Submit/i));
     expect(handleClick).toHaveBeenCalledTimes(1);
     expect(setRequestBody).toHaveBeenCalledTimes(1);
-  });
-
-  it(`sees what is being typed into the textbox`, () => {
-    render(<Textbox />);
-    userEvent.type(screen.getByLabelText(/Text/i), "Hello,{enter}World!");
-    expect(screen.getByLabelText(/Text/i)).toHaveValue("Hello,\nWorld!");
   });
 });
