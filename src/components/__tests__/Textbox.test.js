07b4a615541bf1b3f23517f9742d1e904daf45a8
Jasche7 <jasonc1172@gmail.com>
2021-05-05 17:20:49 -0700

    Changed use of queryBy to getBy

--- src/components/__tests__/Textbox.test.js
+++ src/components/__tests__/Textbox.test.js
@@ -4,23 +4,23 @@ import userEvent from "@testing-library/user-event";
 
 describe(`The Textbox component`, () => {
   it(`includes all of its form inputs`, () => {
-    const { queryByLabelText } = render(<Textbox />);
+    const { getByLabelText } = render(<Textbox />);
 
-    const text = queryByLabelText(/Text/);
+    const text = getByLabelText(/Text/);
     expect(text).toBeInTheDocument();
     expect(text.textContent).toBe("");
 
-    const min = queryByLabelText(/Min Occurrences:/);
+    const min = getByLabelText(/Min Occurrences:/);
     expect(min).toBeInTheDocument();
 
-    const max = queryByLabelText(/Max Phrase Length:/);
+    const max = getByLabelText(/Max Phrase Length:/);
     expect(max).toBeInTheDocument();
 
-    const caseSensitive = queryByLabelText(/Case Sensitive/);
+    const caseSensitive = getByLabelText(/Case Sensitive/);
     expect(caseSensitive).toBeInTheDocument();
     expect(caseSensitive.checked).toEqual(false);
 
-    const ignoresPunctuation = queryByLabelText(/Ignore Punctuation/);
+    const ignoresPunctuation = getByLabelText(/Ignore Punctuation/);
     expect(ignoresPunctuation).toBeInTheDocument();
     expect(ignoresPunctuation.checked).toEqual(false);
   });
