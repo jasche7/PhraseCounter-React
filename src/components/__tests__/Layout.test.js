856e045bcb75a8536e6c0daa8776cf7121fe1424
Jasche7 <jasonc1172@gmail.com>
2021-05-17 21:22:36 -0700

    Removed unnecessary imports

--- src/components/__tests__/Layout.test.js
+++ src/components/__tests__/Layout.test.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
 import Layout from "../Layout";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe(`The Layout component`, () => {
